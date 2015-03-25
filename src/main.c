d4be96a2cd532d7dc9d8308fcd7a2c8feaf5b473
SirGFM <gfmandaji@hotmail.com>
2015-03-24 23:22:59 -0300

    Fixed bug if the zoom wasn't on the config file

--- src/main.c
+++ src/main.c
@@ -59,24 +59,25 @@ int main(int argc, char *argv[]) {
     GFraMe_assertRet(rv == GFraMe_ret_ok, "Error opening file", __ret);
     pSv = &sv;
     rv = GFraMe_save_read_int(&sv, "zoom", &zoom);
-    GFraMe_assertRet(rv == GFraMe_ret_ok, "Error writing variable", __ret);
+    if (rv == GFraMe_ret_ok) {
+        // Switch the resolution
+        if (zoom != 0 && zoom != 2) {
+            GFraMe_ret rv;
+            
+            rv = GFraMe_screen_set_window_size(SCR_W*zoom, SCR_H*zoom);
+            if (rv == GFraMe_ret_ok)
+                GFraMe_screen_set_pixel_perfect(0, 1);
+        }
+        else if (zoom == 0) {
+            GFraMe_ret rv;
+            
+            rv = GFraMe_screen_setFullscreen();
+            if (rv == GFraMe_ret_ok)
+                GFraMe_screen_set_pixel_perfect(0, 1);
+        }
+    }
     GFraMe_save_close(&sv);
     pSv = 0;
-    // Switch the resolution... in real time! (yep, a great idea...)
-    if (zoom != 0 && zoom != 2) {
-        GFraMe_ret rv;
-        
-        rv = GFraMe_screen_set_window_size(SCR_W*zoom, SCR_H*zoom);
-        if (rv == GFraMe_ret_ok)
-            GFraMe_screen_set_pixel_perfect(0, 1);
-    }
-    else if (zoom == 0) {
-        GFraMe_ret rv;
-        
-        rv = GFraMe_screen_setFullscreen();
-        if (rv == GFraMe_ret_ok)
-            GFraMe_screen_set_pixel_perfect(0, 1);
-    }
     
     rv = GFraMe_audio_player_init();
     GFraMe_assertRet(rv == GFraMe_ret_ok, "Audio player init failed", __ret);
